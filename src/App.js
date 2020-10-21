9519f2b3ac3ea7b642192b87f2d6bea941bf979a
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-10-20 12:00:00 -1200

    rotating text fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { useEffect} from 'react';
-// import './App.css';
 import Axios from 'axios'
 import './reset.scss'
 import './App.scss'
+import {setGlitchedWriter, glitchWrite} from "glitched-writer"
 
 
 import YouTube from 'react-youtube';
-import { render } from '@testing-library/react';
 
-function App (){
+ function App (){
  
   // const getReq = () => {
   //   Axios.get('http://localhost:5000/api/requests')
@@ -39,26 +38,63 @@ function App (){
         start: 4
       }
     };
-    
+
+    const wait = time => new Promise(resolve => setTimeout(resolve, time))
+    const glitch = React.createRef()
+    const loginRef = React.createRef()
+
+    // const firstWrite = (element, secFunc) => {
+    //   glitchWrite(element , "Discord")
+    //   wait(6000)
+    //   .then(()=> secFunc(element, firstWrite))
+    // }
+
+    // const secondWrite = (element, firFunc) => {
+    //   glitchWrite(element , "Login")
+    //   wait(6000)
+    //   .then(()=> firFunc(element, secondWrite))
+    // }
+    const testWriter = async (element) => {
+      const displayWriter = setGlitchedWriter(element, {preset: 'nier'})
+      let index = 0
+      const messages = ["bitches", "boats", "hoes"]
+      while(0 != 1){
+        await wait(6000)
+        displayWriter.write(messages[index], {preset: 'nier'})
+        index++
+        if(index === messages.length){
+          index = 0
+        }
+      }
+
+   
+      
+    }
+
+    useEffect(() => {
+      testWriter(glitch.current)
+    }, [])
+
   return (
     <>
       <nav className="nav">
         <div className="link">
-          <a href="google.com">discord login</a>
+          <a ref={loginRef} href="https://www.google.com">discord login</a>
         </div>
-       
       </nav>
-
-    <div className="video-background">
-      <div className="video-foreground">
-      <YouTube
+      <div className="wrapper">
+      <h1 ref={glitch} className="text"></h1> 
+    </div>
+      <div className="video-background">
+        <div className="video-foreground">
+          <YouTube
             videoId="5jmALV5kDVw"
             opts={videoOptions}
             className="video-iframe"
             onReady={(event)=> event.target.mute()}
           />
+        </div>
       </div>
-    </div>
     </>
   );
 }
