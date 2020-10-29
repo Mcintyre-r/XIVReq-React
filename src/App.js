4e06f3c963a9d90c47330504f70214b90991bacd
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-10-28 12:00:00 -1200

    added logo, and crystal scroll

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Axios from 'axios'
 import './reset.scss'
 import './App.scss'
 import {setGlitchedWriter, glitchWrite} from "glitched-writer"
+import logo from './Assets/ffxiv_req_logo.png'
 
 
 import YouTube from 'react-youtube';
@@ -41,25 +42,16 @@ import YouTube from 'react-youtube';
 
     const wait = time => new Promise(resolve => setTimeout(resolve, time))
     const glitch = React.createRef()
-    const loginRef = React.createRef()
+    const crystal_01 = React.createRef()
+    const crystal_02 = React.createRef()
+    const crystal_03 = React.createRef()
 
-    // const firstWrite = (element, secFunc) => {
-    //   glitchWrite(element , "Discord")
-    //   wait(6000)
-    //   .then(()=> secFunc(element, firstWrite))
-    // }
-
-    // const secondWrite = (element, firFunc) => {
-    //   glitchWrite(element , "Login")
-    //   wait(6000)
-    //   .then(()=> firFunc(element, secondWrite))
-    // }
     const testWriter = async (element) => {
       const displayWriter = setGlitchedWriter(element, {preset: 'nier'})
       let index = 0
-      const messages = ["bitches", "boats", "hoes"]
+      const messages = ["A FFXIV work order system", "Login via discord to continue"]
       while(0 != 1){
-        await wait(6000)
+        await wait(18000)
         displayWriter.write(messages[index], {preset: 'nier'})
         index++
         if(index === messages.length){
@@ -71,20 +63,52 @@ import YouTube from 'react-youtube';
       
     }
 
+
+    const scrollCrystal = () => {
+      var posY1 = 0;
+      var posY2 = 0;
+      var posY3 = 0;
+      var imgH = 500;
+      setInterval( function(){
+        if (posY1 <= -900) posY1 = 0;
+        if (posY2 <= -900) posY2 = 0;
+        if (posY3 <= -1200) posY3 = 0;
+        posY1 -= 1;
+        posY2 -= 2;
+        posY3 -= 3;
+        // await crystal_01
+        crystal_01.current.style.backgroundPosition = '0' + posY1 + 'px' 
+        crystal_02.current.style.backgroundPosition = '0' + posY2 + 'px'
+        crystal_03.current.style.backgroundPosition = '0' + posY3 + 'px'
+        // $('.crystal_01').css({ backgroundPosition: '0' + posY1 + 'px' });
+        // $('.crystal_02').css({ backgroundPosition: '0' + posY2 + 'px' });
+        // $('.crystal_03').css({ backgroundPosition: '0' + posY3 + 'px' });
+      },50);
+
+    } 
+    
     useEffect(() => {
       testWriter(glitch.current)
+      scrollCrystal()
     }, [])
 
   return (
     <>
       <nav className="nav">
         <div className="link">
-          <a ref={loginRef} href="https://www.google.com">discord login</a>
+          <a href="https://www.google.com">discord login</a>
         </div>
       </nav>
       <div className="wrapper">
-      <h1 ref={glitch} className="text"></h1> 
-    </div>
+        <img src={logo}/>
+        <h1 ref={glitch} className="text"></h1> 
+      </div>
+      <div id="crystals">
+        <div ref={crystal_01} class="crystal_01"></div>
+        <div ref={crystal_02} class="crystal_02"></div>
+        <div ref={crystal_03} class="crystal_03"></div>    
+      </div>
+  
       <div className="video-background">
         <div className="video-foreground">
           <YouTube
