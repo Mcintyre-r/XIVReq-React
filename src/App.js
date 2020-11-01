b0ddeffb0e4e6ef474f18d96458e2db900d5b846
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-10-31 12:00:00 -1200

    cleaning up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect} from 'react';
 import Axios from 'axios'
 import './reset.scss'
 import './App.scss'
-import {setGlitchedWriter, glitchWrite} from "glitched-writer"
+import {scrollCrystal,testWriter} from './utility.js'
 import logo from './Assets/ffxiv_req_logo.png'
 
 
@@ -19,13 +19,15 @@ import YouTube from 'react-youtube';
   //     console.log("err:", err)
   //   })
   // }
-  // useEffect(()=>{
-  //     getReq()
-  // }, [])
+
   // var decodedCookie = decodeURIComponent(document.cookie);
   // const cookies = document.cookie
   // console.log(cookies)
 
+    const glitch = React.createRef()
+    const crystal_01 = React.createRef()
+    const crystal_02 = React.createRef()
+    const crystal_03 = React.createRef()
     const videoOptions = { 
       playerVars: { // https://developers.google.com/youtube/player_parameters
         autoplay: 1,
@@ -40,52 +42,17 @@ import YouTube from 'react-youtube';
       }
     };
 
-    const wait = time => new Promise(resolve => setTimeout(resolve, time))
-    const glitch = React.createRef()
-    const crystal_01 = React.createRef()
-    const crystal_02 = React.createRef()
-    const crystal_03 = React.createRef()
-
-    const testWriter = async (element) => {
-      const displayWriter = setGlitchedWriter(element, {})
-      let index = 0
-      const messages = ["Login via discord to continue", "A FFXIV work order system"]
-      await wait(3000)
-      displayWriter.write("A FFXIV work order system", {})
-      while(0 != 1){
-        await wait(18000)
-        displayWriter.write(messages[index], {})
-        index++
-        if(index === messages.length){
-          index = 0
-        }
-      }
-
-   
-      
-    }
+    
+    
 
 
-    const scrollCrystal = () => {
-      var posY1 = 0;
-      var posY2 = 0;
-      var posY3 = 0;
-      setInterval( function(){
-        if (posY1 <= -900) posY1 = 0;
-        if (posY2 <= -900) posY2 = 0;
-        if (posY3 <= -1200) posY3 = 0;
-        posY1 -= 1;
-        posY2 -= 2;
-        posY3 -= 3;
-        crystal_01.current.style.backgroundPosition = '0' + posY1 + 'px' 
-        crystal_02.current.style.backgroundPosition = '0' + posY2 + 'px'
-        crystal_03.current.style.backgroundPosition = '0' + posY3 + 'px'
-      },50);
-    } 
+
+
+
     
     useEffect(() => {
       testWriter(glitch.current)
-      scrollCrystal()
+      scrollCrystal(crystal_01,crystal_02,crystal_03)
     }, [])
 
   return (
@@ -96,8 +63,8 @@ import YouTube from 'react-youtube';
         </div>
       </nav>
       <div className="wrapper">
-        <img src={logo} className="fade_in_left"/>
-        <h1 ref={glitch} className="text"></h1> 
+        <img src={logo} className="fade_in_left" alt='FFXIV REQUISITION logo'/>
+        <h1 ref={glitch} className="text" alt='subtitle'> </h1> 
       </div>
       <div id="crystals">
         <div ref={crystal_01} class="crystal_01"></div>
---
+++ src/utility.js
@@ -0,0 +1,36 @@
+import {setGlitchedWriter, glitchWrite} from "glitched-writer"
+
+const wait = time => new Promise(resolve => setTimeout(resolve, time))
+
+export const testWriter = async (element) => {
+    const displayWriter = setGlitchedWriter(element, {})
+    let index = 0
+    const messages = ["Login via discord to continue", "A FFXIV work order system"]
+    await wait(3000)
+    displayWriter.write("A FFXIV work order system", {})
+    while(0 != 1){
+      await wait(18000)
+      displayWriter.write(messages[index], {})
+      index++
+      if(index === messages.length){
+        index = 0
+      }
+    }
+  }
+
+export const scrollCrystal = (crystal_01,crystal_02,crystal_03) => {
+    var posY1 = 0;
+    var posY2 = 0;
+    var posY3 = 0;
+    setInterval( function(){
+      if (posY1 <= -900) posY1 = 0;
+      if (posY2 <= -900) posY2 = 0;
+      if (posY3 <= -1200) posY3 = 0;
+      posY1 -= 1;
+      posY2 -= 2;
+      posY3 -= 3;
+      crystal_01.current.style.backgroundPosition = '0' + posY1 + 'px' 
+      crystal_02.current.style.backgroundPosition = '0' + posY2 + 'px'
+      crystal_03.current.style.backgroundPosition = '0' + posY3 + 'px'
+    },50);
+  } 
