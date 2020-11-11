03234d58b290f9a832104628efcfea3448312efb
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-11-10 12:00:00 -1200

    cleaned up video loop

--- src/Components/backgroundVideo.js
+++ src/Components/backgroundVideo.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import YouTube from 'react-youtube';
 
+
+
 const videoOptions = { 
     playerVars: { // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
       controls: 0,
       rel: 0,
       showinfo: 0,
-      loop: 1,
       modestbranding: 1,
       disablekb: 1,
-      playlist: "5jmALV5kDVw",
-      start: 4
+      start: 4,
+      end: 158,
     }
   };
 
@@ -24,6 +25,7 @@ const backgroundVideo = () =>{
             opts={videoOptions}
             className="video-iframe"
             onReady={(event)=> event.target.mute()}
+            onEnd={(event)=> event.target.seekTo(4)}
             />
         </div>
     </div>
