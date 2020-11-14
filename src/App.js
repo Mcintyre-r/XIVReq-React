7119440a507556e5a74d9f71e4f140f6c36ce0d4
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-11-13 12:00:00 -1200

    started to setup state, fixed ref issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState,useRef} from 'react';
 import './reset.scss'
 import './App.scss'
 import {scrollCrystal,testWriter} from './utility.js'
@@ -9,30 +9,34 @@ import Crystal from './Components/crystal.js'
 
 
  function App (){
+   const [login, setLogin] = useState(false)
+
 
     const glitch = React.createRef()
-    const crystal_01 = React.createRef()
-    const crystal_02 = React.createRef()
-    const crystal_03 = React.createRef()
+    const crystal_01 = useRef()
+    const crystal_02 = useRef()
+    const crystal_03 = useRef()
 
 
     useEffect(() => {
       testWriter(glitch.current)
       scrollCrystal(crystal_01,crystal_02,crystal_03)
-    })
+    },[])
 
   return (
     <div className="container">
       <div className="wrapper fade_in_left">
         <img src={logo} className="" alt='FFXIV REQUISITION logo'/>
         <h1 ref={glitch} className="glitch" alt='subtitle'> </h1> 
+        <button onClick={()=> setLogin(!login)}>Log in</button>
       </div>
 
       <Crystal crystals={[crystal_01,crystal_02,crystal_03]}/>
 
       
       <div className="leftShape"/>
-      <div className="rightShape"/>
+      {login? <div className="rightShape"/> : null}
+      
 
       <Youtube />
     </div>
