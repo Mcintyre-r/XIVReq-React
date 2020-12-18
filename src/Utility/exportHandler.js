d11dd44a038ed58c2059b3ce2549cea5a4464375
Ronald McIntyre <ronxcamacho@gmail.com>
2020-12-17 20:27:44 -0500

    console.logs for debugging

--- src/Utility/exportHandler.js
+++ src/Utility/exportHandler.js
@@ -8,11 +8,14 @@ const exportHandler = (list) => {
         importString = importString+addition
       } else if(request.set){
         const i = request.setItems
+        console.log(i)
         const importSetup = `${i.wpnID},null,1;${i.headID},null,1;${i.chestID},null,1;${i.handsID},null,1;${i.legsID},null,1;${i.feetID},null,1;${i.beltID},null,1;${i.earID},null,1;${i.neckID},null,1;${i.wristID},null,1;${i.ringID},null,2;`
+        console.log(importSetup)
         importString = importString+importSetup
         if(i.shdID){
           importString = importString+`${i.shdID},null,1;`
         }
+        console.log(importString)
       
       }
 
