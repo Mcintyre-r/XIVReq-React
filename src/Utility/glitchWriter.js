1722b276d202e48a9db0a91c22ecef75d2dccbb2
Ronald McIntyre <ronxcamacho@gmail.com>
2022-08-17 21:06:45 -0400

    glitch writing update

--- src/Utility/glitchWriter.js
+++ src/Utility/glitchWriter.js
@@ -5,9 +5,9 @@ const wait = time => new Promise(resolve => setTimeout(resolve, time))
 const glitchWriter = async (element) => {
     const displayWriter = setGlitchedWriter(element, {})
     let index = 0
-    const messages = ["Thank you for using XIV Req", "Direct any feedback to Exa#0469", "Alpha Version 0.001"]
+    const messages = ["Thank you for using XIV Req", "Submit any feedback via the bot.", "Beta Version 0.2"]
     await wait(3000)
-    displayWriter.write("Alpha Version 0.001", {})
+    displayWriter.write("Beta Version 0.2", {})
     while(0 != 1){
       await wait(18000)
       displayWriter.write(messages[index], {})
