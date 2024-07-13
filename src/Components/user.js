74d381ff93064ea955eccb9e5bbabe38091de538
Ronald McIntyre <59479270+Mcintyre-r@users.noreply.github.com>
2024-07-13 07:19:44 -0400

    Update user.js

--- src/Components/user.js
+++ src/Components/user.js
@@ -3,7 +3,7 @@ import React from 'react'
 const user = (props) => {
     return(
         <div className='userCard'>
-            <h2>{props.userName}</h2>
+            <h2 className='userInfo'>{props.userName}</h2>
             <img className='userAvatar' src={props.userAvatar} alt='user'/>
         </div>
     )
