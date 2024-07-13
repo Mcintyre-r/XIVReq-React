d3a69b892fc5aa4193dfc4cbb6e921dbf63bd81d
Ronald McIntyre <59479270+Mcintyre-r@users.noreply.github.com>
2024-07-13 07:41:19 -0400

    username discriminator removal

--- src/Components/cards.js
+++ src/Components/cards.js
@@ -22,7 +22,7 @@ return(
                     :<></>}
                     </div>
                     <div className='Crafter'>
-                        Requester: {props.request.requestedBy+'#'+props.request.requesterDiscriminator}
+                        Requester: props.request.requestedBy
                     </div>
                     <div className='Crafter'>
                         Crafter: {props.request.claimed? props.request.worker : 'unclaimed'}
